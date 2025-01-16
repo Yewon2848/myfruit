16a999317eb73e1568d0b9fa00dd73f823082941
YEWON2848 <yewon3353@gmail.com>
2025-01-16 11:47:41 +0900

    itemDto 연결

--- src/main/java/com/myfruit/pms/dto/ItemDto.java
+++ src/main/java/com/myfruit/pms/dto/ItemDto.java
@@ -2,12 +2,17 @@ package com.myfruit.pms.dto;
 
 public class ItemDto {
     // 필드 (속성)
+    private Integer id;
     private String item;
 
     // 기본 생성자 (필수)
     public ItemDto() {}
 
     // Getter 와 Setter
+    public void setId(Integer id){
+        this.id =id;
+    }
+
     public String getItem() {
         return item;
     }
