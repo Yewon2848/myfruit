2daa6b2e21ba5c99700d65838f2a14631b3eb2d7
YEWON2848 <yewon3353@gmail.com>
2025-01-17 12:36:37 +0900

    상세보기 예외처리 및 목록보기 구현

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -1,9 +1,10 @@
 package com.myfruit.pms.controller;
 
 import com.myfruit.pms.dto.ItemDto;
-import com.myfruit.pms.mapper.ItemMapper;
+import com.myfruit.pms.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -11,22 +12,38 @@ import org.springframework.web.bind.annotation.*;
 public class ItemController {
 
     @Autowired
-    private ItemMapper itemMapper;
+    private ItemService itemService;
 
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "shop/create-item";
     }
 
-//    @GetMapping
-//    public void createItem2(@RequestBody ItemDto itemDto){
-//        System.out.println(itemDto.getItem());
-//    }
-
     @PostMapping
     @ResponseBody
-    public void createItem(@RequestBody ItemDto itemDto){
+    public void createItem(@RequestBody ItemDto itemDto) {
         System.out.println(itemDto.getItem());
-        itemMapper.insertItem(itemDto);
+        itemService.createItem(itemDto);
     }
+
+    @GetMapping("/{id}")
+    public String getItem(@PathVariable("id") int id, Model model) {
+        ItemDto itemDto = itemService.getItem(id);
+        model.addAttribute("item", itemDto);
+
+        return "shop/detail";
+    }
+
+    // 생성 페이지 GET /items/create
+    // 생성 POST /items
+    // 상세보기 GET /items/{id}
+    // 수정 POST /items/{id}
+    // 삭제 GET /items/{id}/delete
+
+    // 생성 페이지 GET /items/create
+    // 생성 POST /items
+    // 상세보기 GET /items/{id}
+    // 수정 PUT /items/{id}
+    // 삭제 DELETE /items/{id}
+
 }
